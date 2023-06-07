3658f6789c30e0c306697349c2207eb8fb24c975
skabeo <spencerokyere95@gmail.com>
2023-06-07 14:46:33 +0100

    Create owners and species table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,35 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species varchar(100);
+
+-- Create owners table
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+-- Create species table
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    PRIMARY KEY(id)
+);
+
+-- Animal table modifications
+
+  -- deleting species column
+    ALTER TABLE animals
+    DROP COLUMN species;
+
+  -- adding species_id column
+    ALTER TABLE animals
+    ADD COLUMN species_id INT REFERENCES species(id);
+
+  -- adding owner_id column
+    ALTER TABLE animals
+    ADD COLUMN owner_id INT REFERENCES owners(id);
+
